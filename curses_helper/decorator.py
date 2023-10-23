1083289e586837a44b28fcdab563fe7fd6477ba7
taitep <arvid@h.se>
2023-10-23 15:17:54 +0200

    Add color initialization to the decorator

--- curses_helper/decorator.py
+++ curses_helper/decorator.py
@@ -1,16 +1,22 @@
 import curses
 
 
-def curses_app(echo=False, cbreak=True, keypad=False):
+def curses_app(echo=False, cbreak=True, keypad=False, color=True):
     def wrapper(func: callable):
         def wrapper(*args, **kwargs):
             try:
                 stdscr = curses.initscr()
+
                 if not echo:
                     curses.noecho()
+
                 if cbreak:
                     curses.cbreak()
+
                 stdscr.keypad(keypad)
+
+                if curses.has_colors() and color:
+                    curses.start_color()
 
                 func(stdscr, *args, **kwargs)
             finally:
